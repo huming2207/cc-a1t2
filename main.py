49fa08c728785f5dbf913138d54e867e320d389e
Jackson Ming Hu <huming2207@gmail.com>
2020-03-22 17:39:21 +1100

    server: add student info model

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 # [END imports]
 
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+DEFAULT_STUDENT_INFO_NAME = 'default_student_info'
 
 
 # We set a parent key on the 'Greetings' to ensure that they are all
@@ -38,27 +38,20 @@ DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 # will be consistent. However, the write rate should be limited to
 # ~1/second.
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+def student_info_key(guestbook_name=DEFAULT_STUDENT_INFO_NAME):
     """Constructs a Datastore key for a Guestbook entity.
 
     We use guestbook_name as the key.
     """
-    return ndb.Key('Guestbook', guestbook_name)
+    return ndb.Key('Students', guestbook_name)
 
 
-# [START greeting]
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
+class StudentInfo(ndb.Model):
+    student_id = ndb.StringProperty(indexed=False)
+    name = ndb.StringProperty(indexed=False)
+    password = ndb.IntegerProperty()
 
 
-class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    country = ndb.StringProperty()
 # [END greeting]
 
 
@@ -66,63 +59,14 @@ class Greeting(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
+        student = StudentInfo(id="s3554025", name="Ming Hu", password=123456)
+        student.put()
 
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
 # [END main_page]
-
-
-# [START guestbook]
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.country = self.request.get('country')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-# [END guestbook]
 
 
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
 ], debug=True)
 # [END app]
--- model.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import ndb
-
-
-class StudentInfo(ndb.Model):
-    id = ndb.KeyProperty(),
-    name = ndb.StringProperty(indexed=False),
-    password = ndb.StringProperty(indexed=False),
